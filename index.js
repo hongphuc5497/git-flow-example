7cb9a281d2967e17a9e5015f37b780ba45b69da5
Hong Phuc <hongphuc.dthp@gmail.com>
2022-10-11 20:41:50 +0700

    feat: create new route

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express();
+const crypto = require('crypto')
 
 app.get('/', (req, res) => {
   res.status(200).json({
     message: 'This is root route'
+  })
+})
+
+app.get('/random-hash', (req, res) => {
+  res.status(200).json({
+    content: crypto.createHash('sha256').update(crypto.randomBytes(16)).digest('hex')
   })
 })
 
